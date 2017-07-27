661a917c45afef7d3a7826b3f69f02426d85a021
aleksandr.st.novikov <aleksandr.st.novikov@gmail.com>
2017-07-27 15:55:00 +0300

    Service - Added Service project, configured installer.

---
+++ Portal.Service/Service.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Portal.Service
+{
+    public partial class Service : ServiceBase
+    {
+        private System.Timers.Timer timer = null;
+
+        public Service()
+        {
+            InitializeComponent();
+
+            timer = new System.Timers.Timer(60000);//создаём объект таймера 
+            timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+    }
+}
